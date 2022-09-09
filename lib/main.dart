8ea08e4b2356f25ba45cfe746ee2e70b0ad53371
Jesse Luoto <jesse.luoto@gmail.com>
2022-09-09 23:24:34 +0300

    Fix title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calc-O-Pad',
       theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Roboto'),
       home: const MyHomePage(title: 'Calc-O-Pad'),
     );
